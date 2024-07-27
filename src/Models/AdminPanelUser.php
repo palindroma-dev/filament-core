2addbe99d0a10ed18535cfa573da6ddc5e72f9df
ucha <ucha4964@gmail.com>
2024-07-27 09:57:26 +0400

    Installed core dependencies

---
+++ src/Models/AdminPanelUser.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Filament\Core\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Spatie\Permission\Traits\HasRoles;
+
+class AdminPanelUser extends Authenticatable implements FilamentUser
+{
+  use HasFactory, Notifiable, hasRoles;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'phone',
+    'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array
+  {
+    return [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
+    ];
+  }
+
+  public function canAccessPanel(Panel $panel): bool
+  {
+    return true;
+  }
+}
