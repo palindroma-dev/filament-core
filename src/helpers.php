408326ce397da707b903ac03556a80223eba64ad
ucha <ucha4964@gmail.com>
2024-08-16 12:07:45 +0400

    Navigation improvements and fund page

--- src/helpers.php
+++ src/helpers.php
@@ -19,6 +19,19 @@ function getNavigationLink(array $item): string {
   }
 }
 
+function isNavigationActive(array $item, \Z3d0X\FilamentFabricator\Models\Page $page): bool
+{
+  $data = $item['data'];
+
+  if($item['type'] == 'page' && isset($data['page_slug'])) {
+    return $data['page_slug'] == $page->slug;
+  } else if (!preg_match('/^https?:\/\//', $data['url']) || preg_match('/^\#/', $data['url'])) {
+    return trim($data['url'], '/') == trim($page->slug, '/');
+  }
+
+  return false;
+}
+
 function internalLink($slug): string
 {
   return URL::to('/') . '/' . app()->getLocale() . '/' . $slug;
