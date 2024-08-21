668c4bb511b0c58b6ee8e6e02c9b91d64c5f9c1c
ucha <ucha4964@gmail.com>
2024-08-21 10:39:06 +0400

    Bug fix in admin panel provider

--- src/Concerns/EditRecord/Translatable.php
+++ src/Concerns/EditRecord/Translatable.php
@@ -63,7 +63,23 @@ trait Translatable
             $localeData = $this->mutateFormDataBeforeSave($localeData);
 
             foreach (Arr::only($localeData, $translatableAttributes) as $key => $value) {
-                $record->setTranslation($key, $locale, $value);
+              if(is_array($value) && (array_values($value)[0] ?? null)) {
+                $value = array_values($value);
+              }
+
+              if(is_array($value)) {
+                foreach($value as &$item) {
+                  if(is_array($item)) {
+                    foreach($item as $key2 => $value2) {
+                      if(is_array($value2) && (array_values($value2)[0] ?? null)) {
+                        $item[$key2] = array_values($value2)[0];
+                      }
+                    }
+                  }
+                }
+              }
+
+              $record->setTranslation($key, $locale, $value);
             }
         }
 
